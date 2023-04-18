1ccfd305f5d0f981e8d27adf597140e8d8577d1c
tranducthang511 <ams.w5112001@gmail.com>
2023-04-19 00:05:45 +0700

    login_new

---
+++ PRJ301_Assignment/src/java/model/Student.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class Student {
+       private int id;
+    private String Name;
+    private Date DOB;
+    private boolean gender;
+    private String email;
+    private int specializedId;
+    private int campusId;
+    private int parentId;
+}
